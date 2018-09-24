ec8f6884614b21acaf3054bbc3e3036e3607192f
Lianqing/Bluto <lqqu@thoughtworks.com>
2018-09-24 21:24:37 +0800

    first init project (add all depency)

---
+++ src/main/java/com/thoughtworks/spring/learning/springwithaop/SpringwithaopApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.spring.learning.springwithaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithaopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwithaopApplication.class, args);
+    }
+}
