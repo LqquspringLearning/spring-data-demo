11c982e518b58958d1445246ebb34fe09630124c
Lianqing/Bluto <lqqu@thoughtworks.com>
2018-09-24 22:33:01 +0800

    finish spring data demo

---
+++ src/main/java/com/thoughtworks/spring/learning/springwithaop/Repositorys/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.spring.learning.springwithaop.Repositorys;
+
+import com.thoughtworks.spring.learning.springwithaop.Entitys.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
+}
