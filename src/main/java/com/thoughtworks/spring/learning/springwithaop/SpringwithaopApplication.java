53b8e99813e2b1aa07f4303c0e37a0624f476a4a
Lianqing/Bluto <lqqu@thoughtworks.com>
2018-09-25 22:43:11 +0800

    remove useless code

--- src/main/java/com/thoughtworks/spring/learning/springwithaop/SpringwithaopApplication.java
+++ src/main/java/com/thoughtworks/spring/learning/springwithaop/SpringwithaopApplication.java
@@ -1,18 +1,11 @@
 package com.thoughtworks.spring.learning.springwithaop;
 
-import com.thoughtworks.spring.learning.springwithaop.Entitys.Customer;
-import com.thoughtworks.spring.learning.springwithaop.Repositorys.CustomerRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 public class SpringwithaopApplication {
-    private static final Logger log = LoggerFactory.getLogger(SpringwithaopApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringwithaopApplication.class, args);
